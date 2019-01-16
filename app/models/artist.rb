1ad69322ee18bea3ad5a2e017bc643d4fc03e3b0
Heloise Taillet <heloise96@hotmail.com>
2019-01-16 14:59:13 -0500

    my tests work

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,62 @@
+require 'pry'
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.collect {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.collect {|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    total = 0
+    self.all.each {|artist| total += artist.years_experience.to_i}
+    total.to_i
+  end
+
+  def self.most_prolific
+    avg = 0.0
+    the_artist = []
+    self.all.each do |artist|
+      p = artist.paintings.length.to_f
+      yoc = artist.years_experience
+      new_avg = (p/yoc).to_f.round(2)
+        if new_avg > avg
+          the_artist = []
+          avg = new_avg
+          the_artist << artist
+        elsif new_avg == avg
+          the_artist << artist
+        end
+    end
+    if the_artist.length == 1
+      the_artist[0]
+    elsif the_artist.length > 1
+      the_artist
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    price = 0.0
+    self.all.each {|painting| price += painting.price.to_f.round(2)}
+    price.to_i
   end
 
 end

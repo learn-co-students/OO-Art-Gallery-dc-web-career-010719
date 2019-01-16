513c41a558b26f76f17018df70e029b3fbb1bb14
gillsinaglass <rdgill95@gmail.com>
2019-01-16 18:10:49 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,64 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    artist_paintings = []
+    Painting.all.each do |painting|
+      if painting.artist == self
+        artist_paintings << painting
+      end
+    end
+  return artist_paintings
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    years = 0
+    @@all.each do |artist|
+      years += artist.years_experience
+    end
+    years
+  end
+
+  def self.most_prolific
+    highest_amount = 0
+    highest_instance = nil
+    @@all.each do |artist|
+      test = (artist.paintings.count.to_f / artist.years_experience)
+       if test > highest_amount
+         highest_amount = test
+         highest_instance = artist
+       end
+     end
+     return highest_instance
+  end
+
+  def create_painting(title, price, gallery)
+    painting = Painting.new(title, price, self, gallery)
+    return painting
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,30 @@ require_relative '../config/environment.rb'
 
 
 
-binding.pry
+# binding.pry
 
 puts "Bob Ross rules."
+
+ryan = Artist.new("Ryan", 7)
+matt = Artist.new("Matt", 2)
+puts Artist.all == [ryan, matt]
+louve = Gallery.new("Louve", "Paris")
+art_house = Gallery.new("Art House", "New York")
+mona_lisa = Painting.new("Mona Lisa", 50000, ryan, louve)
+sunshine = Painting.new("Sunshine", 60000, matt, art_house)
+requiem = Painting.new("Requiem", 1000, ryan, art_house)
+ryan.create_painting("Alabama Rain", 12000, art_house)
+
+puts ryan.paintings.count == 3
+puts ryan.galleries == [louve, art_house, art_house]
+puts matt.paintings == [sunshine]
+puts matt.galleries == [art_house]
+puts ryan.cities == ["Paris", "New York"]
+puts Artist.total_experience == 9
+puts Artist.most_prolific == matt
+puts Painting.all.count == 4
+puts Painting.total_price == 123000
+puts louve.paintings == [mona_lisa]
+puts art_house.artists == [matt, ryan]
+puts art_house.artist_names == [matt.name, ryan.name]
+puts art_house.most_expensive_painting == sunshine

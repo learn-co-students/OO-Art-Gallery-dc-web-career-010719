0fe684b64ca764b1b567f34fdf630a9cd63bd89e
github username <github email address>
2019-01-15 22:45:06 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,80 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    total = 0
+    self.all.each do |exp|
+      total += exp.years_experience
+    end
+    total
+  end
+
+  def self.most_prolific
+    most_pro = nil
+    most_amount = 0
+    self.all.each do |artist|
+      ppy = artist.paintings.size.to_f / artist.years_experience.to_f
+      if ppy > most_amount
+        most_pro = artist
+        most_amount = ppy
+      end
+    end
+    most_pro
+  end
+
+  def create_painting(title, price, gallery_object)
+    Painting.new(title, price, self, gallery_object)
+  end
+
+  def paintings
+    portfolio = []
+    Painting.all.each do |painting|
+      if painting.artist == self
+        portfolio << painting
+      end
+    end
+    portfolio
+  end
+
+  def galleries
+    gals = []
+    Painting.all.each do |painting|
+      if painting.artist == self
+        gals << painting.gallery
+      end
+    end
+    gals.uniq
+  end
+
+  def cities
+    locs = []
+    self.galleries.each do |gal|
+      locs << gal.city
+    end
+    locs.uniq
   end
 
 end
+
+
+
+
+
+
+
+
+
+# Artist#cities
+# Return an array of all cities that an artist has paintings in
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,36 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.gallery == self}
+  end
+
+  def artists
+    artists_in_gallery = []
+    self.paintings.each do |painting|
+      artists_in_gallery << painting.artist
+    end
+    artists_in_gallery.uniq
+  end
+
+  def artist_names
+    self.artists.map{|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.sort_by{|painting| painting.price}.reverse[0]
   end
 
 end

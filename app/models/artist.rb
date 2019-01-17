ec8755a25fe41700e42af138fdc7e0b5d51e282f
dcjones1 <dcejjones@gmail.com>
2019-01-16 23:30:32 -0500

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,43 @@
 class Artist
-
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    self.paintings.map {|painting| painting.gallery.city}
+  end
+
+  def self.total_experience
+    self.all.sum {|artist| artist.years_experience}
+  end
+
+  def self.most_prolific
+    paintings_per_year = []
+    Artist.all.each_with_index do |artist, index|
+      count = Artist.all.count {|artist| artist.paintings}
+      paintings_per_year << count.to_f/artist.years_experience
+    end
+    Artist.all[paintings_per_year.index(paintings_per_year.max)]
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,33 @@
 require_relative '../config/environment.rb'
 
+#initialize
+picasso = Artist.new("Pablo", 15)
+dali = Artist.new("Salvador", 10)
+louvre = Gallery.new("The Louvre", "Paris")
+tate = Gallery.new("Tate Modern", "London")
+guitarist = Painting.new("The Old Guitarist", 15000, picasso, louvre)
+time = Painting.new("The Persistence of Time", 30000, dali, tate)
+horse = dali.create_painting("horse", 20000, louvre)
 
+#artist tests
+puts Artist.all == [picasso, dali]
+puts dali.paintings == [time, horse]
+puts dali.galleries == [tate, louvre]
+puts dali.cities == ["London", "Paris"]
+puts Artist.total_experience == 25
+puts Artist.most_prolific == dali
+
+#painting tests
+puts Painting.all == [guitarist, time, horse]
+puts Painting.total_price == 65000
+
+
+#gallery tests
+puts Gallery.all == [louvre, tate]
+puts louvre.paintings == [guitarist, horse]
+puts louvre.artists == [picasso, dali]
+puts louvre.artist_names == ["Pablo", "Salvador"]
+puts louvre.most_expensive_painting == horse
 
 
 binding.pry
